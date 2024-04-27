95dc62ac1188e2b04a937a60ce3ae7ae17b4b500
Aser Eid <asereid56@gmail.com>
2024-04-27 21:51:04 +0300

    Initial Commit

---
+++ SportSpot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportSpot
+//
+//  Created by Aser Eid on 27/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
